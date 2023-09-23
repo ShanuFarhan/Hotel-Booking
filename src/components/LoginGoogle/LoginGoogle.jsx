74f607ddad5a48007a367e0b317acd1270c490f1
Shanu Farhan <64779709+ShanuFarhan@users.noreply.github.com>
2023-09-23 15:19:09 +0530

    Update LoginGoogle.jsx

--- src/components/LoginGoogle/LoginGoogle.jsx
+++ src/components/LoginGoogle/LoginGoogle.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { auth,provider } from "../../Firebase/config" 
 import { signInWithPopup } from 'firebase/auth'
-import { Container,TextField, Button,Box, Typography } from '@mui/material';
+import { Container,Button,Box, Typography } from '@mui/material';
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./LoginGoogle.css"
-import Home from '../../Pages/Home/Home'
-import { Link, useNavigate } from 'react-router-dom'
+import {useNavigate } from 'react-router-dom'
 import {Row, Col} from 'react-bootstrap'
 const LoginGoogle = () => {
     const navigate=useNavigate()
@@ -32,34 +31,7 @@ const LoginGoogle = () => {
             navigate("/home")
         ):(
     <>
-          {/* <form  style={{height:400,width:'50%'}}>
-        <Box sx={{alignContent:'space-around',height:500,width:500,margin:'110px 50px 0 50px'}}>
-      <Typography v
-      style={{color:'black',fontWeight:550,marginTop:50,marginLeft:170}} variant='h4'>Sign in</Typography>
-      
-      <input style={{border:'none',paddingLeft:15,height:45,fontSize:14,color:'black',borderRadius:10,marginLeft:50,width:350,marginTop:30}}
-        placeholder="Email"
-        name="email"
-        type='email' */}
-        {/* // value={formData.email}
-        // onChange={handleChange} */}
         
-      {/* /> */}
-        {/* {errors.email && <Typography marginLeft="30px" variant='subtitle' color="error">{errors.email}</Typography>} */}
-
-      {/* <input style={{border:'none',paddingLeft:15,height:45,fontSize:14,color:'black',borderRadius:10,marginLeft:50,width:350,marginTop:30}}
-        placeholder="Password"
-        name="password"
-        type='password'
-        // value={formData.password}
-        // onChange={handleChange}
-      />
-        {/* {errors.password && <Typography marginLeft="30px" variant='subtitle' color="error">{errors.password}</Typography>} */}
-
-      {/* <Button type="submit" variant="contained" onClick={handleLogin} 
-      style={{fontWeight:550,backgroundColor:'white',color:'black',margin:'40px 0 0 50px',width:350}}>
-        Login
-      </Button> */} 
       <Container maxWidth="sm">
       <Box
         sx={{
@@ -84,9 +56,6 @@ const LoginGoogle = () => {
       style={{fontWeight:550,backgroundColor:'white',color:'black',margin:'40px 0 0 50px',width:350}}>
         Login
       </Button> 
-      
-    
-   
             <Row className='form mt-4'>
               <Col className="text-center">
                 <Typography style={{fontWeight:550,margin:'20px 220px',marginLeft:'250px'}}>or</Typography>
@@ -98,9 +67,6 @@ const LoginGoogle = () => {
             </Row>
       </Box>   
       </Container>
-    
-    
-    
             </>
         )}
     </div>
@@ -108,3 +74,4 @@ const LoginGoogle = () => {
   )
 }
 export default LoginGoogle
+
